b2d09f93ac2424c696c59338ada5048df6450f94
Lukas Fridl <lukas@fridl.cz>
2021-03-09 21:32:39 +0100

    Third

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  home
+//
+//  Created by Lukas Fridl on 09.03.2021.
+//
+
+#include <iostream>
+#include "prolific.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    Prolific board;
+    board.GetRelays();
+
+    return 0;
+}
